af5090a39b587ac2495ef4d4d99e8dc91e771bbd
KKU <dkdlwkrsbxjs@naver.com>
2022-12-06 22:33:31 +0900

    토글버튼까지 작업 완료

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,107 @@
 <template>
   <div>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <header id="header" class="">
+      <section id="navTop" class="nav-top py-2 d-none d-md-block">
+        <div class="container">
+          <div class="row align-items-center justify-content-center">
+            <div class="col-4"></div>
+            <div class="col-4 logo">
+              <img src="https://img.freepik.com/premium-vector/company-logo-design-ideas-vector-free-vector_536813-247.jpg?w=1380" class="rounded mx-auto d-block" alt="" style="height: 150px;">
+            </div>
+            <div class="col-4 text-end socials">
+              <div class="sign-container">
+                <li v-if="user.email==undefined"><a type="button" class="login" @click="kakaoLogin" style="color:black">Kakao-Login</a></li>
+                <!-- <li v-if="user.email==undefined"><button class="btn btn-danger" type="button" @click="kakaoLogin">로그인</button></li> -->
+                <!-- <li v-else><button class="btn btn-danger" type="button" @click="kakaoLogout">로그아웃</button></li> -->
+                <li v-else><a type="button" class="logout" @click="kakaoLogout" style="color:black">Logout</a></li>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <nav id="navPrimary" class="navbar navbar-expand-md bg-dark">
+        <div class="container-xxl">
+          <a class="navbar-brand d-block d-md-none" href="index.html">
+            <!-- <img src="https://img.freepik.com/premium-vector/company-logo-design-ideas-vector-free-vector_536813-247.jpg?w=1380" class="rounded mx-auto d-block" alt="" style="height: 50px;"> -->
+          </a>
+          <div class="collapse navbar-collapse" id="navPrimaryContent">
+              <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="index.html" style="color:white">Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color:white">
+                    Intro
+                  </a>
+                  <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="page.html">About us</a></li>
+                    <li><a class="dropdown-item" href="page.html">Find us</a></li>
+                  </ul>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="page.html" style="color:white">BEST</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="page.html" style="color:white">NEW</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="page.html" style="color:white">OUTER</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="page.html" style="color:white">SHITRS</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="page.html" style="color:white">PANTS</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="page.html" style="color:white">SALE</a>
+                </li>
+                <!-- <li class="nav-item">
+                  <a class="nav-link btn btn-outline-secondary" href="#"><i class="fa fa-search"></i></a>
+                </li> -->
+              </ul>
+            </div>
+            <!-- 토글버튼 -->
+            <button class="btn btn-outline-secondary d-block d-md-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+              <span class="navbar-toggler-icon">ㅡ</span>
+            </button>
+            <!-- 오른쪽 상단 메뉴 표시 -->
+            <div class="offcanvas offcanvas-end d-block d-md-none" data-bs-scroll="true" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+              <div class="offcanvas-header">
+                <h5 class="offcanvasRightLabel">Menu</h5>
+                <button class="btn-close text-reset" type="button" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+              </div>
+              <div class="offcanvas-body">
+                <ul class="list-unstyled mobile-menu">
+                  <li><a href="index.html" class="text-decoration-none link-dark active">Home</a></li>
+                  <li>
+                    <a href="page.html" class="text-decoration-none link-dark">Intro</a>
+                    <ul class="list-unstyled ms-3">
+                      <li><a href="page.html" class="text-decoration-none link-dark">About us</a></li>
+                      <li><a href="page.html" class="text-decoration-none link-dark">Find us</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="page.html" class="text-decoration-none link-dark"></a></li>
+                  <li><a href="page.html" class="text-decoration-none link-dark">BEST</a></li>
+                  <li><a href="page.html" class="text-decoration-none link-dark">NEW</a></li>
+                  <li><a href="page.html" class="text-decoration-none link-dark">OUTER</a></li>
+                  <li><a href="page.html" class="text-decoration-none link-dark">SHIRTS</a></li>
+                  <li><a href="page.html" class="text-decoration-none link-dark">PANTS</a></li>
+                  <li><a href="page.html" class="text-decoration-none link-dark">SALE</a></li>
+                  <li><a href="#" class="btn btn-outline-secondary w-100"><i class="fa fa-search"></i></a></li>
+                </ul>
+              </div>
+            </div>
+        </div>
+        
+      </nav>
+
+    </header>
+    <!-- <div class="main-logo">
+      <img src="https://img.freepik.com/premium-vector/company-logo-design-ideas-vector-free-vector_536813-247.jpg?w=1380" class="rounded mx-auto d-block" alt="" style="height: 150px;">
+    </div> -->
+          <!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container-fluid">
               <a class="navbar-brand" href="#">FPP</a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -30,7 +131,7 @@
                 </form>
               </div>
             </div>
-          </nav>
+          </nav> -->
   <router-view/>
   <footer class="mt-5 py-5 bg-dark">
             <div class="row">
@@ -129,7 +230,7 @@ export default {
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -148,5 +249,46 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+} */
+@font-face {
+    font-family: 'LINESeedKR-Bd';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_11-01@1.0/LINESeedKR-Bd.woff2') format('woff2');
+    font-weight: 700;
+    font-style: normal;
 }
+@font-face {
+    font-family: 'MICEGothic Bold';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2206-01@1.0/MICEGothic Bold.woff2') format('woff2');
+    font-weight: 700;
+    font-style: normal;
+}
+
+.login {
+  font-family: 'LINESeedKR-Bd';
+  font-size: 1.3rem;
+}
+
+.logout {
+  font-family: 'LINESeedKR-Bd';
+  font-size: 1.3rem;
+}
+
+.nav-link {
+  font-family: 'MICEGothic Bold';
+  font-size: 1rem;
+}
+
+.nav-link active {
+  font-family: 'MICEGothic Bold';
+  font-size: 1rem;
+}
+
+/* 토글버튼 안 글씨 */
+.text-decoration-none {
+  font-family: 'MICEGothic Bold';
+}
+
+
+
+
 </style>
